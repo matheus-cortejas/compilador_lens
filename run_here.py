b1b7eed49813f511fcce6cc9c5e12530dfc4d079
Augusto Gilioli <augustolg9@gmail.com>
2025-04-22 16:06:05 -0400

    Refazendo

--- run_here.py
+++
@@ -1,8 +0,0 @@
-from analisador_lexico.scanner import scanner
-
-with open("script.lens", "r", encoding="utf-8") as file:
-    codigo = file.read()
-
-tokens = scanner(codigo)
-for t in tokens:
-    print(t)
